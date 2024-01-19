2216721ad50e33e15f5fa33c178ecaf766eeffcd
Clara Prioux <prioux.clara@gmail.com>
2024-01-19 17:15:23 +0000

    design in progress

--- index.php
+++ index.php
@@ -13,15 +13,17 @@
     <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Raleway:wght@100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Raleway:wght@300;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <div class="main-section">
        <div class="tasks-section">
         <h2>To-do list</h2>
             <form action="" method="POST">
-                Title: <input type="text" name="title"><br>
-                Description: <input type="text" name="description"><br>
+                <input type="text" name="title" placeholder="Enter task title..."><br>
+                <textarea type="text" name="description" id="description" class="no-bootstrap-styles" placeholder="Enter task description..."></textarea><br>
                 <button type="submit" name="submit" value="Submit">Add +</button>
             </form>
 </body>
@@ -140,7 +142,8 @@
                     
                         echo   "<td class=\"todo-title\">" . $result['title'] . "</td>
                                 <td class=\"todo-description\">" . $result['description'] . "</td>";
-                        echo "<td class=\"todo-X\"><a href=\"delete.php?id=" . $result['id'] . "\">x</a><a href=\"update.php?id=" . $result['id'] . "\"> edit</a></td>";        
+                        echo "<td class=\"todo-X\"><a href=\"delete.php?id=" . $result['id'] . "\">X</a></td>"; 
+                        echo "<td class=\"todo-X\"><a href=\"update.php?id=" . $result['id'] . "\">edit</a></td>";        
                         echo  "</tr>";
                     }  
                 ?>
